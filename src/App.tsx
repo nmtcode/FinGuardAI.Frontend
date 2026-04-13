62eeb66457a0f9bf6bfd0b34a7b8857e31255e5a
nmtcode <nawaf92004@gmail.com>
2026-04-13 11:42:04 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-slate-900">
+      <h1 className="text-4xl font-bold text-sky-400 underline">
+        Hello world! Vite + Tailwind is live.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
